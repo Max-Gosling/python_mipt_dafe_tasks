13a728557377b63277c68501599a4d4913289f2b
Prosto_Max_xX <maxim.nevinsky@yandex.ru>
2026-03-11 18:49:06 +0300

    remade tasks for les03

--- solutions/sem02/lesson03/task1.py
+++ solutions/sem02/lesson03/task1.py
@@ -8,13 +8,26 @@ class ShapeMismatchError(Exception):
 def sum_arrays_vectorized(
     lhs: np.ndarray,
     rhs: np.ndarray,
-) -> np.ndarray: ...
+) -> np.ndarray:
+    if len(lhs) != len(rhs):
+        raise ShapeMismatchError
+    return lhs + rhs
 
 
-def compute_poly_vectorized(abscissa: np.ndarray) -> np.ndarray: ...
+def compute_poly_vectorized(abscissa: np.ndarray) -> np.ndarray:
+    return np.power(abscissa, 2) * 3 + abscissa * 2 + 1
 
 
 def get_mutual_l2_distances_vectorized(
     lhs: np.ndarray,
     rhs: np.ndarray,
-) -> np.ndarray: ...
+) -> np.ndarray:
+    if lhs.shape != rhs.shape:
+        raise ShapeMismatchError
+    return [
+        [
+            sum((lhs[i][k] - rhs[j][k]) ** 2 for k in range(len(lhs[0]))) ** 0.5
+            for j in range(len(rhs))
+        ]
+        for i in range(len(lhs))
+    ]
