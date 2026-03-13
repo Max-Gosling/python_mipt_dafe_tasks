159eb977f33c752699702cc6853ea3b60ddd9dd2
Prosto_Max_xX <maxim.nevinsky@yandex.ru>
2026-03-13 21:55:51 +0300

    made tasks for les04

--- solutions/sem02/lesson04/task1.py
+++ solutions/sem02/lesson04/task1.py
@@ -1,17 +1,52 @@
 import numpy as np
 
+MIN_PAD_SIZE = 1
+MIN_KERNEL_SIZE = 1
+
 
 def pad_image(image: np.ndarray, pad_size: int) -> np.ndarray:
-    # ваш код
-    return image
+    if pad_size < MIN_PAD_SIZE:
+        raise ValueError
+    shp = image.shape
+    if len(shp) == 2:
+        new_shp = (shp[0] + 2 * pad_size, shp[1] + 2 * pad_size)
+        padded_image = np.zeros(shape=new_shp, dtype=image.dtype)
+        for i in range(shp[0]):
+            for j in range(shp[1]):
+                padded_image[i + pad_size, j + pad_size] = image[i, j]
+    else:
+        new_shp = (shp[0] + 2 * pad_size, shp[1] + 2 * pad_size, shp[2])
+        padded_image = np.zeros(shape=new_shp, dtype=image.dtype)
+        for i in range(shp[0]):
+            for j in range(shp[1]):
+                for k in range(shp[2]):
+                    padded_image[i + pad_size, j + pad_size, k] = image[i, j, k]
+    return padded_image
 
 
 def blur_image(
     image: np.ndarray,
     kernel_size: int,
 ) -> np.ndarray:
-    # ваш код
-    return image
+    if (kernel_size < MIN_KERNEL_SIZE) or (kernel_size % 2 == 0):
+        raise ValueError
+    if kernel_size == 1:
+        return image
+    shp = image.shape
+    pad = kernel_size // 2
+    image = pad_image(image, pad)
+    blured_image = np.zeros(shape=shp, dtype=image.dtype)
+    if len(shp) == 2:
+        for i in range(shp[0]):
+            for j in range(shp[1]):
+                kemel = image[i : i + kernel_size, j : j + kernel_size]
+                blured_image[i, j] = np.mean(kemel)
+    else:
+        for i in range(shp[0]):
+            for j in range(shp[1]):
+                kemel = image[i : i + kernel_size, j : j + kernel_size, :]
+                blured_image[i, j, :] = np.mean(kemel, axis=(0, 1))
+    return blured_image
 
 
 if __name__ == "__main__":
--- solutions/sem02/lesson04/task2.py
+++ solutions/sem02/lesson04/task2.py
@@ -1,10 +1,24 @@
 import numpy as np
 
+MINIMAL_THRESHOLD = 1
+
 
 def get_dominant_color_info(
     image: np.ndarray[np.uint8],
     threshold: int = 5,
 ) -> tuple[np.uint8, float]:
-    # ваш код
-
-    return 0, 0
+    if threshold < MINIMAL_THRESHOLD:
+        raise ValueError("threshold must be positive")
+    all_possibals = np.full(shape=256, fill_value=-42, dtype=int)
+    image = image.reshape(-1)
+    for pixel in range(0, 256):
+        if pixel in image:
+            all_possibals[pixel] = 0
+        mask = (image > pixel) & ((image - pixel) < threshold) | (image <= pixel) & (
+            (pixel - image) < threshold
+        )
+        all_possibals[pixel] += np.sum(mask)
+    return (
+        np.uint8(np.argmax(all_possibals)),
+        all_possibals[np.argmax(all_possibals)] / image.size,
+    )
