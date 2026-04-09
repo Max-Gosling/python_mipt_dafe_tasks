68094c74bab62d6824892b2d6ec20329014b164c
Prosto_Max_xX <maxim.nevinsky@yandex.ru>
2026-04-09 21:27:22 +0300

    made tasks for les7

--- solutions/sem02/lesson07/task1.py
+++ solutions/sem02/lesson07/task1.py
@@ -13,9 +13,123 @@ def visualize_diagrams(
     ordinates: np.ndarray,
     diagram_type: Any,
 ) -> None:
-    # ваш код
-    pass
+    if abscissa.size != ordinates.size:
+        raise ShapeMismatchError
+    plt.style.use("ggplot")
+    if diagram_type == "hist":
+        bins_amount = abscissa.size // 20
+        figure = plt.figure(figsize=(9, 9))
+        grid = plt.GridSpec(4, 4)
 
+        axis_scatter = figure.add_subplot(grid[:-1, 1:])
+        axis_hist_vert = figure.add_subplot(
+            grid[:-1, 0],
+            sharey=axis_scatter,
+        )
+        axis_hist_hor = figure.add_subplot(
+            grid[-1, 1:],
+            sharex=axis_scatter,
+        )
+
+        axis_scatter.scatter(abscissa, ordinates, color="purple", alpha=0.5)
+        axis_hist_hor.hist(
+            abscissa,
+            bins=bins_amount,
+            color="cornflowerblue",
+            edgecolor="blue",
+            density=True,
+            alpha=0.7,
+        )
+        axis_hist_vert.hist(
+            ordinates,
+            bins=bins_amount,
+            color="indianred",
+            edgecolor="red",
+            orientation="horizontal",
+            density=True,
+            alpha=0.7,
+        )
+
+        axis_hist_hor.invert_yaxis()
+        axis_hist_vert.invert_xaxis()
+    elif diagram_type == "box":
+        figure = plt.figure(figsize=(9, 9))
+        grid = plt.GridSpec(4, 4)
+
+        axis_scatter = figure.add_subplot(grid[:-1, 1:])
+        axis_box_vert = figure.add_subplot(
+            grid[:-1, 0],
+            sharey=axis_scatter,
+        )
+        axis_box_hor = figure.add_subplot(
+            grid[-1, 1:],
+            sharex=axis_scatter,
+        )
+
+        axis_scatter.scatter(abscissa, ordinates, color="cornflowerblue", alpha=0.5)
+        axis_box_hor.boxplot(
+            abscissa,
+            vert=False,
+            patch_artist=True,
+            boxprops=dict(facecolor="lightsteelblue"),
+            medianprops=dict(color="k")
+        )
+        axis_box_vert.boxplot(
+            ordinates, 
+            vert=True,
+            patch_artist=True,
+            boxprops=dict(facecolor="lightsteelblue"),
+            medianprops=dict(color="k")
+        )
+        axis_box_hor.invert_yaxis()
+        axis_box_vert.invert_xaxis()
+    elif diagram_type == "violin":
+        figure = plt.figure(figsize=(9, 9))
+        grid = plt.GridSpec(4, 4)
+
+        axis_scatter = figure.add_subplot(grid[:-1, 1:])
+        axis_viol_vert = figure.add_subplot(
+            grid[:-1, 0],
+            sharey=axis_scatter,
+        )
+        axis_viol_hor = figure.add_subplot(
+            grid[-1, 1:],
+            sharex=axis_scatter,
+        )
+
+        axis_scatter.scatter(abscissa, ordinates, color="purple", alpha=0.5)
+        parts_hor = axis_viol_hor.violinplot(
+            abscissa,
+            vert=False,
+            showmedians=True,
+            )
+        for body in parts_hor["bodies"]:
+            body.set_facecolor("cornflowerblue")
+            body.set_edgecolor("blue")
+
+        for part in parts_hor:
+            if part == "bodies":
+                continue
+            parts_hor[part].set_edgecolor("cornflowerblue")
+
+        parts_viol = axis_viol_vert.violinplot(
+            ordinates,
+            vert=True,
+            showmedians=True,
+            )
+        for body in parts_viol["bodies"]:
+            body.set_facecolor("indianred")
+            body.set_edgecolor("red")
+
+        for part in parts_viol:
+            if part == "bodies":
+                continue
+            parts_viol[part].set_edgecolor("indianred")
+        axis_viol_hor.invert_yaxis()
+        axis_viol_vert.invert_xaxis()
+    else:
+        raise ValueError
+    
 
 if __name__ == "__main__":
     mean = [2, 3]
--- solutions/sem02/lesson07/task2.py
+++ solutions/sem02/lesson07/task2.py
@@ -1,1 +1,39 @@
-# ваш код (используйте функции или классы для решения данной задачи)
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+
+def download_data(path: str) -> tuple[np.ndarray, np.ndarray]:
+    with open(path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    before = np.array(data['before'])
+    after = np.array(data['after'])
+    return (before, after)
+
+def count_stages(arr: np.ndarray) -> list[int]:
+    counts = []
+    for stage in ["I", "II", "III", "IV"]:
+        mask = (arr == stage)
+        counts.append(sum(mask))
+    return counts
+
+def visualize_data(before: list[int], after: list[int]):
+    plt.style.use("ggplot")
+    stages = ["I", "II", "III", "IV"]
+    poses = np.arange(len(stages))
+    width = 0.4
+
+    _, axes = plt.subplots(figsize=(9, 6))  # задаём размер
+    axes.bar(poses - width/2, before, width, label="before", color="cornflowerblue", edgecolor="blue")
+    axes.bar(poses + width/2, after, width, label="after", color="indianred", edgecolor="red")
+    axes.set_xticks(poses)
+    axes.set_xticklabels(stages)
+    axes.set_ylabel("Amount of people")
+    axes.set_title("Mitral disease stages")
+    axes.legend()
+
+
+if __name__ == "__main__":
+    before, after = download_data(path="solutions/sem02/lesson07/data/medic_data.json")
+    visualize_data(count_stages(before), count_stages(after))
+    plt.show()
+
